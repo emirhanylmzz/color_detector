265c7ce132c2f26e425eeccd66b56ed342af3858
Emirhan <emirhan.y2073@hotmail.com>
2020-02-14 01:34:43 +0300

    Update color detector.py

--- color detector.py
+++ color detector.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Feb 13 23:12:01 2020
-
 @author: emirhanylmzz
 """
-
 import cv2
 import numpy as np
 
 video_capture = cv2.VideoCapture(0)
 
 #blue and red colors
-boundiries=[
+boundaries=[
         ([161, 155, 84], [179, 255, 255]), 
         ([94, 80, 2], [126, 255, 255])]
 color = 0
+
 while True:
     _, frame = video_capture.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    lower = np.array(boundiries[color][0])
-    upper = np.array(boundiries[color][1])
+    lower = np.array(boundaries[color][0])
+    upper = np.array(boundaries[color][1])
     
     mask = cv2.inRange(hsv, lower, upper)
     o = cv2.bitwise_and(frame, frame, mask=mask)
